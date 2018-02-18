c8645dd669fbba4db23e490ee85dd66d0d7eefb7
Romin Irani <romin.irani@xoriant.com>
2018-02-18 16:38:47 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+// Imports the Google Cloud client library
+const language = require('@google-cloud/language');
+
+// Instantiates a client
+const client = new language.LanguageServiceClient();
+
+// The text to analyze
+const text = 'Google Natural Language API is fantastic. It gives me great results and I love it.';
+
+const document = {
+  content: text,
+  type: 'PLAIN_TEXT',
+};
+
+// Detects the sentiment of the text
+client
+  .analyzeSentiment({document: document})
+  .then(results => {
+    const sentiment = results[0].documentSentiment;
+
+    console.log(`Text: ${text}`);
+    console.log(`Sentiment score: ${sentiment.score}`);
+    console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
