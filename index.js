08e14d445de04bae4c9022cb2c7f52d515ddcc79
Romin Irani <romin.irani@xoriant.com>
2018-02-18 15:26:49 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const language = require('@google-cloud/language')({
-    projectId: '<your-project-id>',
-    keyFilename: 'key.json'
-    });
-
-language.detectSentiment('Google Natural Language API is a fantastic API which gives highly accurate results. I love it.',{'verbose':true}).then((result) => {
-    console.log('Score:', result[0].score);
-    console.log('Magnitude:', result[0].magnitude);
-    });
